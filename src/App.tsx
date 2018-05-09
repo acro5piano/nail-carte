dde59a967d425c20ccd96a3f55885391acb2062e
gosho-kazuya <ketsume0211@gmail.com>
2018-05-09 19:55:30 +0900

    init stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 
 export default class App extends React.Component {
   render() {
-    return <div>Hello World</div>
+    return <div>Hello World!!</div>
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import './index.css'
+import 'app/index.css'
 
 import App from './App'
 
