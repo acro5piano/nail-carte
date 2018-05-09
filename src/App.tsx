bfe5faf63d4cb812e0afc10bc6e8d6d9d6aad42e
gosho-kazuya <ketsume0211@gmail.com>
2018-05-09 20:23:58 +0900

    module resolver

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export default class App extends React.Component {
+export default class App extends React.Component<{}> {
   render() {
     return <div>Hello World!!</div>
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import 'app/index.css'
-
 import App from './App'
 
 ReactDOM.render(
