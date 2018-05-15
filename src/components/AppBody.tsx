9be57c61e7aca570c8f4c968d5b7d4c08470397c
gosho-kazuya <ketsume0211@gmail.com>
2018-05-15 23:04:25 +0900

    add body

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import FloatingActionButton from './material-ui/Button/FloatingActionButton'
 import { withStyles } from 'material-ui/styles'
+import Divider from 'material-ui/Divider'
+import List, { ListItem, ListItemText } from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import ImageIcon from '@material-ui/icons/Image'
 // import { OpenSidebarUseCase } from '../use-case/ToggleAppSidebarUseCase'
 // import { appContextLocator } from '../AppContextLocator'
 
@@ -9,15 +13,35 @@ interface AppBodyProps {}
 const notify = () => alert('hello')
 
 const AppBody = ({ classes }) => (
-  <div className="AppBody">
+  <div className="appBody">
+    <div className={classes.root}>
+      <List>
+        <ListItem>
+          <Avatar><ImageIcon /></Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <li><Divider inset /></li>
+        <ListItem>
+          <Avatar><ImageIcon /></Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <li><Divider inset /></li>
+        <ListItem>
+          <Avatar><ImageIcon /></Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <li><Divider inset /></li>
+      </List>
+    </div>
     <FloatingActionButton onClick={notify} />
   </div>
 )
 
 const styles = {
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
+  root: {
+    marginTop: 24,
+    backgroundColor: '#fff',
+    padding: 12,
   },
 }
 
