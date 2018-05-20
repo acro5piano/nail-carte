7c34009bac2bd828e0fb4080b68b2ddaafaca4b3
gosho-kazuya <ketsume0211@gmail.com>
2018-05-20 18:35:16 +0900

    change project structure

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 import FloatingActionButton from 'sarte/components/material-ui/Button/FloatingActionButton'
 import { withStyles } from 'material-ui/styles'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Customers from 'sarte/components/pages/Customers/Customers'
-import NewCustomer from 'sarte/components/pages/Customers/NewCustomer'
-import NotFound from 'sarte/components/pages/NotFound'
+import Customers from 'sarte/components/Customer/CustomerList'
+import NewCustomer from 'sarte/components/Customer/CreateCustomer'
+import NotFound from 'sarte/components/NotFound'
 
 interface AppBodyProps {}
 
