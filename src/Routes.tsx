af31f45da7d2b3d91bb0bb157b02292c0cef7d3f
gosho-kazuya <ketsume0211@gmail.com>
2018-05-20 23:50:23 +0900

    linkify

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import FloatingActionButton from 'sarte/components/MaterialUi/Button/FloatingActionButton'
-import Customers from 'sarte/components/Customer/CustomerList'
+import CustomerList from 'sarte/components/Customer/CustomerList'
 import NewCustomer from 'sarte/components/Customer/CreateCustomer'
 import NotFound from 'sarte/components/NotFound'
-
-const notify = () => alert('hello')
 
 export const HOME_PATH = '/'
 export const CUSTOMER_LIST_PATH = '/customers'
@@ -18,11 +15,10 @@ const Routes = (props) => {
 
   return (
     <div className="appBody">
-      <FloatingActionButton onClick={notify} />
       <Router>
         <div>
           <Switch>
-            <Route exact path={CUSTOMER_LIST_PATH} component={Customers}/>
+            <Route exact path={CUSTOMER_LIST_PATH} component={CustomerList}/>
             <Route exact path={NEW_CUSTOMER_PATH} component={NewCustomer}/>
             <RouteWithProps path={HOME_PATH} render={NotFound}/>
           </Switch>
--- src/components/Customer/CustomerList.tsx
+++ src/components/Customer/CustomerList.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react'
 import { withStyles } from 'material-ui/styles'
 import Divider from 'material-ui/Divider'
+import FloatingActionButton from 'sarte/components/MaterialUi/Button/FloatingActionButton'
 import List, { ListItem, ListItemText } from 'material-ui/List'
 import Avatar from 'material-ui/Avatar'
 import ImageIcon from '@material-ui/icons/Image'
+import { NEW_CUSTOMER_PATH } from 'sarte/Routes'
 // import AppHeader from 'sarte/components/AppHeader'
 
 interface CustomersProps {}
 
+const toNewCustomer = () => location.href = '/#/' + NEW_CUSTOMER_PATH
+
 // import Customer from '../entities/Customer'
 // const c = new Customer({name: 'KAZUYA'})
 
-const Customers = ({ classes }) => (
+const CustomerList = ({ classes }) => (
   <div className={classes.root}>
+    <FloatingActionButton onClick={toNewCustomer} />
     <List>
       <ListItem>
         <Avatar><ImageIcon /></Avatar>
@@ -41,4 +46,4 @@ const styles = {
   },
 }
 
-export default withStyles(styles)<CustomersProps>(Customers)
+export default withStyles(styles)<CustomersProps>(CustomerList)
