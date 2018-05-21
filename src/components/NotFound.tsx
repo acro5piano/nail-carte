20ba270ad38ac2dbd023657f947ca2037515ea93
gosho-kazuya <ketsume0211@gmail.com>
2018-05-21 11:51:29 +0900

    upgrade packages

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 import { CUSTOMER_LIST_PATH } from 'sarte/Routes'
 
 const NotFound = () =>
   <div>
     <h3>Not Found</h3>
-    <a href={CUSTOMER_LIST_PATH}>Customers</a>
+    <Link to={CUSTOMER_LIST_PATH}>Customers!!!</Link>
   </div>
 
 export default NotFound
