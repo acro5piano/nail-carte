1bc54a9893c83cb418796154fc7e02c8bad9306a
gosho-kazuya <ketsume0211@gmail.com>
2018-06-03 23:00:10 +0900

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import 'typeface-roboto'
 import './index.css'
 import App from 'sarte/App'
+import register from 'sarte/registerServiceWorker'
 
 if (module.hot) {
   module.hot.accept(() => {
@@ -14,3 +15,4 @@ ReactDOM.render(
   <App />,
   document.getElementById('root'),
 )
+register()
